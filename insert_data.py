541c5c05d0ad093a5e2fcfdcd3709bd1cb351164
Koby Fogel <kobyfogel@hotmail.com>
2020-12-14 20:08:19 +0200

    fixed insert_data

--- insert_data.py
+++ insert_data.py
@@ -7,8 +7,8 @@ with open('7000.csv', encoding="utf-8") as f:
     data=[tuple(line) for line in csv.reader(f)]
 
 
-query = "INSERT INTO wine ('_id', 'country', 'description', 'points', 'province', 'title', 'variety', 'winery', 'user_id') VALUES"
+query = "INSERT INTO wine ('country', 'description', 'points', 'province', 'title', 'variety', 'winery', 'user_id') VALUES"
 for line in data[1:]:
-    new_line = (line[:-1]) + ("NULL",)
+    new_line = (line[1:-1]) + ("NULL",)
     db.session.execute(f"{query} {new_line};")
 db.session.commit()
