8a2e8eae5b1126d2896904691a6968a716d8048e
Koby Fogel <kobyfogel@hotmail.com>
2020-12-14 20:21:44 +0200

    fixed insert_data

--- insert_data.py
+++ insert_data.py
@@ -10,5 +10,5 @@ with open('7000.csv', encoding="utf-8") as f:
 query = "INSERT INTO wine ('country', 'description', 'points', 'province', 'title', 'variety', 'winery', 'user_id') VALUES"
 for line in data[1:]:
     new_line = (line[1:-1]) + ("NULL",)
-    db.session.execute(f"{query} {new_line};")
+    db.session.execute(f"{query} {line};")
 db.session.commit()
