a4e0af90b1d6d522ff74676b8af9a58baf0ccf71
Yam Mesicka <yammesicka@gmail.com>
2020-12-25 00:11:35 +0200

    test: Add tests.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from wine import app
+from wine.wine import app
 
 if __name__ == "__main__":
     app.run()
--- wine/__init__.py
+++ wine/__init__.py
@@ -9,10 +9,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET') or "1e79eb0s3d554c1fc1e979bf3c80dc22"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or "sqlite:///wine.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
 login_manager.login_view = 'login'
 
-from wine import routes
+from wine.wine import routes
